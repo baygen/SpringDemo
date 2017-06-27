31541d2df1061e7d55d80ef04fc68cdb927e939e
Buy <Buy@192.168.0.104>
2017-06-27 18:33:04 +0300

    Initialize commit

---
+++ SpringDemo/src/main/java/com/springdemo/nodes/Company.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springdemo.nodes;
+
+/**
+ *
+ * @author Buy
+ */
+public class Company {
+    
+    private String name;
+    private int earnings;
+    
+}
